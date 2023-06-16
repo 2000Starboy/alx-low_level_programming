3f76cc437644a153e274e1a8fe72411f33500d50
Youness CHAKIRI <chakiriyouness5@hotmail.com>
2023-06-16 16:36:51 -0700

    3, 4

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,30 +1,17 @@
 #include <stdio.h>
-
-
 /**
-
- * main - Prints the alphabet.
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
+* main - Prints the alphabet.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
-
 {
-
 char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-        int i;
-
-
-        for (i = 0; i < 52; i++)
-
-        {
+int i;
+for (i = 0; i < 52; i++)
+{
 putchar(alp[i]);
 }
 putchar('\n');
-return (0);
+return (0);
 }
