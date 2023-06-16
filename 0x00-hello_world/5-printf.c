35e57028f463c92a924e30e8b2f9a1464b2abfb5
Youness CHAKIRI <chakiriyouness5@hotmail.com>
2023-06-15 17:26:00 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
+
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
+
 int main(void)
+
 {
+
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+
+return (0);
+
 }
