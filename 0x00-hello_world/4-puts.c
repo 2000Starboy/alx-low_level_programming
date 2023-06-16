faa7d79ebcff805934b66d7fba7897b492c7e291
Youness CHAKIRI <chakiriyouness5@hotmail.com>
2023-06-15 17:22:32 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Entry point of the program
- *
- * Return: Always 0 (indicating successful execution)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-char *message = "\"Programming is like building a multilingual puzzle";
-puts(message);
-return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
